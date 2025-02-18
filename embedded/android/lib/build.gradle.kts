64a8ab8e56bed71396b6c9d158c6a877ce955bd0
Hugh Nimmo-Smith <hughns@element.io>
2025-02-18 14:22:40 +0000

    Fix package name and try to enable signing

--- embedded/android/lib/build.gradle.kts
+++ embedded/android/lib/build.gradle.kts
@@ -29,12 +29,11 @@ android {
 mavenPublishing {
     publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
 
-    // TODO: get signing working
-    // signAllPublications()
+    signAllPublications()
 
     val version = System.getenv("EC_VERSION")
     // coordinates("io.element.call", "embedded-element-call", version)
-    coordinates("com.github.hughns", "embedded-element-call", version)
+    coordinates("io.github.hughns", "embedded-element-call", version)
     pom {
         name = "Embedded Element Call for Android"
         description.set("Assets needed to embed the Element Call webapp into an Android application.")
